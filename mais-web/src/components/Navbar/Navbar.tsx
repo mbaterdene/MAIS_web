ecc86cea82f41f17ec8f208043cc8f90c588989d
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2025-03-25 21:33:14 +0800

    Added navbar

---
+++ mais-web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "./Menu/Menu";
+import logo from "../../assets/mais_logo_light.png";
+import { IconContext } from "react-icons";
+import { IoSearch } from "react-icons/io5";
+
+const Navbar: React.FC = () => {
+
+  return (
+    <div className="flex flex-row justify-between items-center w-full h-20 bg-white dark:bg-gray-100 p-1 py-1 shadow-md z-30 fixed top-0">
+      <button className="flex flex-row items-center hover:cursor-pointer">
+        <div className="w-16 h-16 ml-10">
+          <img src={logo} alt="logo" className="w-16"/>
+        </div>
+        <Link to="/" className="text-3xl ml-2">
+          Mongol Aspiration
+        </Link>
+      </button>
+
+      <div className="flex flex-row ml-auto mr-4 space-x-4">
+        <div className="flex flex-row items-center">
+          <div className="h-8 border-l p-1 hover:bg-gray-100 border-t border-b border-gray-500 rounded-l-md flex items-center justify-center">
+            <IconContext.Provider value={{ size: "1.5em" }}>
+              <IoSearch />
+            </IconContext.Provider>
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-48 h-8 border border-gray-500 rounded-r-md pl-2 focus:outline-none"
+          />
+        </div>
+
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ mais-web/src/components/ThemeAtom.ts
@@ -0,0 +1,39 @@
+import { atomWithStorage } from "jotai/utils";
+import { atom } from "jotai";
+
+export const Language = atomWithStorage("language", "EN");
+
+export const setLanguage = atom(
+  null, 
+  (get, set, value: string) => {
+    set(Language, value);
+  }
+);
+
+interface NewUser {
+    username: string;
+    email: string;
+    password: string;
+    picture: string;
+    bio: string;
+    badges: string[];
+    achievements: string[];
+  }
+  
+  export const newUser = atomWithStorage<NewUser>("newUser", {
+    username: 'lol',
+    email: '',
+    password: '',
+    picture: '',
+    bio: '',
+    badges: [],
+    achievements: [],
+  });
+
+export const setNewUser = atom(
+  null, 
+  (get, set, { type, value }: { type: keyof NewUser; value: any }) => {
+    const prevUser = get(newUser); 
+    set(newUser, { ...prevUser, [type]: value }); 
+  }
+);
